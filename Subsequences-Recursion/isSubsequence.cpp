c853db6bb7351c273613c065927ba8bf3bf66ee9
Sk-Akibuzzaman-Jony <108605211+Sk-Akibuzzaman-Jony@users.noreply.github.com>
2023-01-01 16:43:11 +0530

    Create isSubsequence.cpp

---
+++ Subsequences-Recursion/isSubsequence.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int solve(int i, int j, string& s, string& t){
+        if (i == 0 || j == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            if (s[i-1] == t[j-1])
+            {
+                return 1+solve(i-1,j-1,s,t);
+            }
+            else
+            {
+                return solve(i,j-1,s,t);
+            }
+        }
+        
+        
+    }
+    bool isSubsequence(string s, string t) {
+        if (s.length() == solve(s.length(), t.length(), s,t))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+        
+        
+    }
+};
