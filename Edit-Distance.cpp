9813e1be5d46f8c94099a95ff3f51cc267db6dbe
Sk-Akibuzzaman-Jony <108605211+Sk-Akibuzzaman-Jony@users.noreply.github.com>
2023-01-06 08:54:26 +0530

    Create Edit-Distance.cpp

---
+++ Edit-Distance.cpp
@@ -0,0 +1,33 @@
+
+//Leet Code Question 72
+//VVI Asked in amazon interview
+
+class Solution {
+public:
+    int solve(string a, string b, int i, int j) {
+        //base
+        if(i == a.length())
+            return b.length() - j;
+        if( j == b.length())
+            return a.length() - i;
+        
+        if(a[i] == b[j] ) {
+            //match
+            return solve(a,b, i+1, j+1);
+        }
+        else {
+            //no match
+            int insertAns = 1 + solve(a,b,i,j+1);
+            int replaceAns = 1 + solve(a,b,i+1,j+1);
+            int deleteAns = 1 + solve(a,b,i+1,j);
+            
+            int finalAns = min(insertAns, min(replaceAns, deleteAns)) ;
+            return finalAns;
+        }
+        
+        
+    }
+    int minDistance(string word1, string word2) {
+        return solve(word1, word2, 0, 0);
+    }
+};
